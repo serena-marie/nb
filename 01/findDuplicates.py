6d03db0bcf05c1fa3e39d02f44ec8a86d88195cf
serena-marie <serena.gutierrez0@gmail.com>
2023-09-23 12:56:38 -0700

    Corrected file to find duplicates. Could be improved upon

--- 01/findDuplicates.py
+++ 01/findDuplicates.py
@@ -1,28 +1,40 @@
-# I should only be adding to the collection if it IS a duplicate
-# If it's the first occurence, need to keep track, if it's the second occurence need to add to list
+# v2, runtime and space to be improved upon
 def findDuplicates(filename):
-  with open(filename, 'r') as file:
-    text= file.read() # file is read in as one big chunk
-    strings = text.split('\n') # stored into a list
-    
-    collection = {}
-    
-    for originalString in strings:
-      sortedString = ''.join(sorted(originalString))
-      existence = collection.get(sortedString)
-      
-      if existence is None: # doesn't exist in collection, might be able to skip variable creation but test it works first
-          collection[sortedString] = originalString
-      """
-      if collection[sortedString]: # this wont work because you'll get a key error
-          continue
-      else
-      """            
-  return collection.items()
-        
-        
+    with open(filename, "r") as file:
+        text = file.read()  # file is read in as one big chunk
+        strings = text.split("\n")  # stored into a list
+
+        collection = {}
+
+        for originalString in strings:
+            sortedString = "".join(sorted(originalString))
+
+            existence = collection.get(sortedString)
+
+            if (
+                existence is None
+            ):  # doesn't exist in collection, might be able to skip variable creation but test it works first
+                collection[sortedString] = {"original": originalString, "freq": 1}
+            else:
+                collection[sortedString]["freq"] = (
+                    collection[sortedString].get("freq") + 1
+                )
+
+        results = []
+
+        for key in collection:
+            if collection[key]["freq"] > 1:
+                results.append(collection[key]["original"])
+        """ runtime error: dictionary changed size during iteration
+        for key in collection:
+            if collection[key]['freq'] == 1:
+                del collection[key]
+        """
+
+    return results
+
 
 try:
-    print(findDuplicates('mininonsense.txt'))
+    print(findDuplicates("nonsense.txt"))
 except FileNotFoundError as e:
-    print(f'Error! File not found. ==> {e}')
+    print(f"Error! File not found. ==> {e}")
